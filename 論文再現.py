d241ed74d405e8b68c58d8df3219559d3d525a86
y-matsuno-u <chyu24003@g.nihon-u.ac.jp>
2024-06-11 19:22:14 +0900

    トレースアウトの実装をしなければ

--- 論文再現.py
+++ 論文再現.py
@@ -1,6 +1,7 @@
 import numpy as np
 import scipy.linalg
 import matplotlib.pyplot as plt
+import qutip as qt
 
 #パラメータ
 L=100 #サイト数
@@ -24,6 +25,7 @@ times = np.linspace(0, t_f, d_t)
 #粒子は最初l=45にいる
 psi[45] = 1  
 
+########################################################################################################################################################
 
 #FIG.3.(a)の再現
 psi_abs=[]#各時刻での各サイトの存在確率を格納するリスト
@@ -54,4 +56,43 @@ plt.colorbar(label='Log Probability Density')
 plt.title('Logarithmic Scale Time Evolution of the State Vector')
 plt.xlabel('Site Index')
 plt.ylabel('Time')
+plt.show()
+
+############################################################################################################################################################
+
+#FIG.3.(b)の再現
+t_b=6/alpha #この時刻で密度行列を計算する
+
+#状態ベクトルの初期化
+psi = np.zeros(L, dtype=complex)
+
+#粒子は最初l=45にいる
+psi[45] = 1  
+
+#時間発展ユニタリ演算子を生成し、時間発展させる
+U_t = scipy.linalg.expm(-1j * H * (t_f/d_t))
+psi = np.dot(U_t, psi)
+
+# 状態ベクトルから密度行列を計算
+rho = np.outer(psi, np.conjugate(psi))
+
+# ホライズン内側を部分トレース
+
+#時刻t_bでのハミルトニアン(horizonの外)の固有ベクトルと固有値を求める
+H_out=H[l_h+1:,l_h+1:]
+eigenvalues, eigenvectors = np.linalg.eigh(H_out)
+
+#E_nの確率を求める
+probabilities=[] #各nに対するE_nの確率を格納するリスト
+for eigenvector in eigenvectors.T:
+    probabilities.append(np.vdot(eigenvector,np.dot(rho_out,eigenvector)))
+
+#plot
+log_probabilities=[]
+log_probabilities=np.log(np.array(probabilities)+1e-30)
+plt.figure(figsize=(6,6))
+plt.xlim(0,10)
+plt.xticks([0,1,2,3,4,5,6,7,8,9,10])
+plt.grid()
+plt.plot(eigenvalues, log_probabilities)
 plt.show()
